d96254553cba6ed6f160a745e0685aa5a502f600
Philipp Kitzberger <philipp@kitze.net>
2019-12-04 11:07:02 +0100

    Add faker for Numeric (integer, float) and Internet (email)

---
+++ Classes/Property/Internet.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace GeorgRinger\Faker\Property;
+
+use Faker\Generator;
+
+class Internet implements PropertyInterface
+{
+    static public function getSettings(array $configuration = [])
+    {
+        return [
+            'type' => self::class,
+            'subtype' => $configuration['subtype'],
+        ];
+    }
+
+    public function generate(Generator $faker, array $configuration = [])
+    {
+        switch ($configuration['subtype']) {
+            case 'email':
+                return $faker->email();
+            default:
+                throw new \Exception('Please specify subtype property!');
+        }
+    }
+}
